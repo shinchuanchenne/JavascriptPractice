9b591b2d11ead7dfb0a7db337a9bd6bc0f5747fc
shinchuanchenne <130829038+shinchuanchenne@users.noreply.github.com>
2023-04-22 11:24:30 +0800

    Delete 6.WhosBuyingLunch.js

--- 6.WhosBuyingLunch.js
+++
@@ -1,6 +0,0 @@
-function whosPlaying(names) {
-  var numberOfPeople = names.length;
-  var randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
-  var randomPerson = names[randomPersonPosition];
-  return randomPerson + " is going to buy lunch today!";
-}
