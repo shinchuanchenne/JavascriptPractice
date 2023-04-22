2979d9e2d26d653312ab61baf5fdeb26d2f809ab
shinchuanchenne <130829038+shinchuanchenne@users.noreply.github.com>
2023-04-22 11:15:21 +0800

    Add files via upload

--- 3.BMICalculators.js
+++ 3.BMICalculators.js
@@ -7,4 +7,11 @@ function bmiCalculator(weight, height) {
 }
 
 var bmi = bmiCalculator(weight, height);
-alert("Your BMI is " + bmi + ".");
+
+if (bmi < 18.5) {
+  alert("Your BMI is " + bmi + " , so you are underweight.");
+} else if (bmi >= 18.5 && bmi < 24.9) {
+  alert("Your BMI is " + bmi + " , so you have a normal weight.");
+} else {
+  alert("Your BMI is " + bmi + " , so you are overweight.");
+}
---
+++ 6.WhosBuyingLunch.js
@@ -0,0 +1,6 @@
+function whosPlaying(names) {
+  var numberOfPeople = names.length;
+  var randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
+  var randomPerson = names[randomPersonPosition];
+  return randomPerson + " is going to buy lunch today!";
+}
